011b3efc7a1f9d80b212c3072299f739db5ea09e
Mallikarjun <mallikarjunkamtam@gmail.com>
2022-01-02 10:38:50 +0530

    created website for this code

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
 // SOUCE OF CHARACTERS
-const letters = ['a', 'b', 'c','d', 'e', 'f','g', 'h', 'i','j', 'k', 'l','m', 'n', 'o','p', 'q', 'r','s', 't', 'u','v', 'w', 'x', 'y', 'z'];
-const numbers = ['0','1','2','3','4','5','6','7','8','9'];
-const symbols = ['!','@','$','#','%','^','&','*','(',')','-','_',];
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ["!", "@", "$", "#", "%", "^", "&", "*", "(", ")", "-", "_"];
 
-const allchar = [letters, numbers, symbols];
-let passwordList = [];
+function passwordGen() {
+  const allchar = [letters, numbers, symbols];
+  let passwordList = [];
+  let size = Number(document.querySelector(".input-box").value);
 
-// ENTER THE SIZE OF PASSWORD HERE
-const size = prompt("Enter the size to password:");
+  let p;
+  for (let i = 1; i <= size; i++) {
+    let q = Math.floor(Math.random() * 3);
+    if (q == 0) {
+      p = Math.floor(Math.random() * 26);
+    } else if (q == 1) {
+      p = Math.floor(Math.random() * 10);
+    } else {
+      p = Math.floor(Math.random() * 12);
+    }
 
-// MIXING THE CHARACTERS HERE
-let p;
-for (let i=0; i<=size; i++){
-    let q = Math.floor((Math.random()*3))
-    if(q == 0){
-     p = Math.floor((Math.random()*26))
-    } else if (q == 1){
-        p = Math.floor((Math.random()*10))
-    }   else {p = Math.floor((Math.random()*12))}
-    
-    passwordList.push(allchar[q][p])
+    passwordList.push(allchar[q][p]);
+    const password = passwordList.join("");
+    document.querySelector(".password").textContent = password;
+  }
 }
 
-// CONVERTING ARRAY INTO STRING
-const password = passwordList.join("");
+function resetfunc() {
+  document.querySelector(".input-box").value = "";
+  document.querySelector(".password").textContent = "";
+}
 
-console.log(password);
+document.querySelector(".submit").addEventListener("click", passwordGen);
+document.querySelector(".reset").addEventListener("click", resetfunc);
